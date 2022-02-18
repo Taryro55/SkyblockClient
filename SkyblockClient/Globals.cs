49943266b5c5f9ceb8d6b27612822f858374ae5e
Taryro55 <b3nja16@protonmail.com>
2022-02-18 00:57:29 -0300

    changed the repo

--- SkyblockClient/Globals.cs
+++ SkyblockClient/Globals.cs
@@ -170,7 +170,7 @@ namespace SkyblockClient
 
 		public static void InitializeValues()
 		{
-			string result = "https://github.com/nacrt/SkyblockClient-REPO/blob/main/files/";
+			string result = "https://github.com/Taryro55/SkyblockClient-REPO/blob/main/files/";
 
 			try
 			{
