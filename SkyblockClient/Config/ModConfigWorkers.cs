88ddcccabfb158575b8121bf9b2c35f4f8c5f9bb
Taryro55 <b3nja16@protonmail.com>
2022-02-22 14:51:53 -0300

    added config downloader

--- SkyblockClient/Config/ModConfigWorkers.cs
+++ SkyblockClient/Config/ModConfigWorkers.cs
@@ -107,4 +107,153 @@ namespace SkyblockClient.Config
 		}
 	}
 
+	[ModConfigWorker("damagetint")]
+	class ModConfigWorkerDamageTint : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("W-OVERFLOW", "W-OVERFLOW/DamageTint");
+			string source = await helper.DownloadFileByte("damagetint.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "DamageTint", "damagetint.toml"));
+		}
+	}
+
+	[ModConfigWorker("patcher")]
+	class ModConfigWorkerPatcher : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("config");
+			string source = await helper.DownloadFileByte("patcher.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockConfigLocation, "patcher.toml"));
+		}
+	}
+
+	[ModConfigWorker("crashpatch")]
+	class ModConfigWorkerCrashPatch : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("W-OVERFLOW", "W-OVERFLOW/CrashPatch");
+			string source = await helper.DownloadFileByte("SKYCLIENT");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "CrashPatch", "SKYCLIENT"));
+		}
+	}
+
+	[ModConfigWorker("hytilities-reborn")]
+	class ModConfigWorkerHytilitiesReborn : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("W-OVERFLOW", "W-OVERFLOW/Hytilities Reborn");
+
+			string source = await helper.DownloadFileByte("blockhighlight.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "Hytilities Reborn", "blockhighlight.toml"));
+
+			string sourceCfg = await helper.DownloadFileByte("hytilitiesreborn.toml");
+			helper.Move(sourceCfg, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "Hytilities Reborn", "hytilitiesreborn.toml"));
+		}
+	}
+
+	[ModConfigWorker("blockoverlay")]
+	class ModConfigWorkerBlockOverlay : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("config");
+
+			string source = await helper.DownloadFileByte("blockoverlay.cfg");
+			helper.Move(source, Path.Combine(Globals.skyblockConfigLocation, "blockoverlay.cfg"));
+		}
+	}
+
+	[ModConfigWorker("nick_hider")]
+	class ModConfigWorkerNickHider : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("config");
+
+			string source = await helper.DownloadFileByte("nickhider.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "nickhider.toml"));
+		}
+	}
+
+	[ModConfigWorker("chatting")]
+	class ModConfigWorkerChatting : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("W-OVERFLOW", "W-OVERFLOW/Chatting");
+
+			string source = await helper.DownloadFileByte("chatshortcuts.json");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "Chatting", "chatshortcuts.json"));
+
+			string sourceTabs = await helper.DownloadFileByte("chattabs.json");
+			helper.Move(sourceTabs, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "Chatting", "chattabs.json"));
+
+			string sourceCfg = await helper.DownloadFileByte("chatting.toml");
+			helper.Move(sourceCfg, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "Chatting", "chatting.toml"));
+		}
+	}
+
+	[ModConfigWorker("redaction")]
+	class ModConfigWorkerRedaction : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("W-OVERFLOW", "W-OVERFLOW/REDACTION");
+
+			string source = await helper.DownloadFileByte("redaction.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "REDACTION", "redaction.toml"));
+		}
+	}
+
+	[ModConfigWorker("keystrokes")]
+	class ModConfigWorkerKeyStrokes : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("config");
+
+			string source = await helper.DownloadFileByte("keystrokes.json");
+			helper.Move(source, Path.Combine(Globals.skyblockConfigLocation, "keystrokes.json"));
+		}
+	}
+
+	[ModConfigWorker("levelhead")]
+	class ModConfigWorkerLevelHead : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("config");
+
+			string source = await helper.DownloadFileByte("levelhead.json");
+			helper.Move(source, Path.Combine(Globals.skyblockConfigLocation, "levelhead.json"));
+		}
+	}
+
+	[ModConfigWorker("lobby_sounds")]
+	class ModConfigWorkerLobbySounds : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("config");
+
+			string source = await helper.DownloadFileByte("lobbysounds.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockConfigLocation, "lobbysounds.toml"));
+		}
+	}
+
+	[ModConfigWorker("tabulous")]
+	class ModConfigWorkerTabulous : ModConfigWorkerBase
+	{
+		public override async Task Work()
+		{
+			helper.InitFolders("W-OVERFLOW", "W-OVERFLOW/Tabulous");
+
+			string source = await helper.DownloadFileByte("tabulous.toml");
+			helper.Move(source, Path.Combine(Globals.skyblockRootLocation, "W-OVERFLOW", "Tabulous", "tabulous.toml"));
+		}
+	}
 }
